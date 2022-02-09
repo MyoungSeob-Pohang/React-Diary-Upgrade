2e3818dc1130997dbcc1016961fcd64beac4a62c
myoungseob <myoungseob91@gmail.com>
2022-02-09 22:13:03 +0900

    New Create

--- src/pages/New.js
+++ src/pages/New.js
@@ -1,8 +1,9 @@
+import DiaryEditor from '../components/DiaryEditor';
+
 const New = () => {
     return (
-        <div className="New">
-            <h1>New</h1>
-            <p>이곳은 일기추가 작성페이지 입니다.</p>
+        <div>
+            <DiaryEditor />
         </div>
     );
 };
